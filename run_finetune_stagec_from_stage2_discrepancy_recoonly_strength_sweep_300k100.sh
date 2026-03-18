95a2623aa6adda80e9fbf04a2a0bf9638ceb0f28
ryanlyang <ryan_l_yang@brown.edu>
2026-03-18 18:07:16 -0400

    yeah

--- run_finetune_stagec_from_stage2_discrepancy_recoonly_strength_sweep_300k100.sh
+++ run_finetune_stagec_from_stage2_discrepancy_recoonly_strength_sweep_300k100.sh
@@ -7,8 +7,7 @@
 #SBATCH -J stgCDRw
 #SBATCH -p tier3
 #SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=48G
+#SBATCH --mem=32G
 #SBATCH -t 2:00:00
 #SBATCH -o offline_reconstructor_logs/stagec_discrepancy_sweeps_recoonly/stagec_disc_recoonly_%j.out
 #SBATCH -e offline_reconstructor_logs/stagec_discrepancy_sweeps_recoonly/stagec_disc_recoonly_%j.err
