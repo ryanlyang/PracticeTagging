bda9efde7c11ca0338c97feb2b5192b954c5fa95
ryanlyang <ryan_l_yang@brown.edu>
2026-03-24 22:59:14 -0400

    good

--- sbatch/reco_teacher_joint_fusion_6model_150k75k150k/run_m6_concat_stagea_corrected_150k75k150k.sh
+++ sbatch/reco_teacher_joint_fusion_6model_150k75k150k/run_m6_concat_stagea_corrected_150k75k150k.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=m6c300
-#SBATCH --partition=tier3
+#SBATCH --partition=debug
 #SBATCH --gres=gpu:1
 #SBATCH --mem=48G
-#SBATCH --time=10:00:00
+#SBATCH --time=8:00:00
 #SBATCH --output=offline_reconstructor_logs/reco_teacher_joint_fusion_6model_150k75k150k/m6_concat_%j.out
 #SBATCH --error=offline_reconstructor_logs/reco_teacher_joint_fusion_6model_150k75k150k/m6_concat_%j.err
 
