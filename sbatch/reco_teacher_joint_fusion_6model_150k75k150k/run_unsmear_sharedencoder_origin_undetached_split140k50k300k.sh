0a60cb119929a38f8f180455347d3d9e6b530773
ryanlyang <ryan_l_yang@brown.edu>
2026-04-04 20:37:41 -0400

    good

--- sbatch/reco_teacher_joint_fusion_6model_150k75k150k/run_unsmear_sharedencoder_origin_undetached_split140k50k300k.sh
+++ sbatch/reco_teacher_joint_fusion_6model_150k75k150k/run_unsmear_sharedencoder_origin_undetached_split140k50k300k.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=tier3
 #SBATCH --gres=gpu:1
 #SBATCH --mem=64G
-#SBATCH --time=8:00:00
+#SBATCH --time=3:00:00
 #SBATCH --output=offline_reconstructor_logs/unsmear_sharedencoder_joint_new/unsmear_sharedencoder_split140k50k300k_%j.out
 #SBATCH --error=offline_reconstructor_logs/unsmear_sharedencoder_joint_new/unsmear_sharedencoder_split140k50k300k_%j.err
 
