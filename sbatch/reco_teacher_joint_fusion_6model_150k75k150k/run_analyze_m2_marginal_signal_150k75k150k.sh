aab612b5595b9a46ba270c88279bdfa3228d7db6
ryanlyang <ryan_l_yang@brown.edu>
2026-04-06 16:16:22 -0400

    good

--- sbatch/reco_teacher_joint_fusion_6model_150k75k150k/run_analyze_m2_marginal_signal_150k75k150k.sh
+++ sbatch/reco_teacher_joint_fusion_6model_150k75k150k/run_analyze_m2_marginal_signal_150k75k150k.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=tier3
 #SBATCH --gres=gpu:1
 #SBATCH --mem=24G
-#SBATCH --time=14:00:00
+#SBATCH --time=02:00:00
 #SBATCH --output=offline_reconstructor_logs/reco_teacher_joint_fusion_6model_150k75k150k/m2_diag_marginal_signal_%j.out
 #SBATCH --error=offline_reconstructor_logs/reco_teacher_joint_fusion_6model_150k75k150k/m2_diag_marginal_signal_%j.err
 
