e5a5f5aa33d25bdf9486dbf0d9df2fc1ac2eee94
ryanlyang <ryan_l_yang@brown.edu>
2026-04-08 16:10:46 -0400

    good

--- sbatch/reco_teacher_joint_fusion_6model_150k75k150k/run_analyze_hlt_joint31_rolling_chunk_greedy_precomputed_1m.sh
+++ sbatch/reco_teacher_joint_fusion_6model_150k75k150k/run_analyze_hlt_joint31_rolling_chunk_greedy_precomputed_1m.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=an31rollpc
-#SBATCH --partition=tier3
+#SBATCH --partition=debug
 #SBATCH --gres=gpu:1
 #SBATCH --mem=64G
-#SBATCH --time=2-00:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --output=offline_reconstructor_logs/reco_teacher_joint_fusion_6model_150k75k150k/analyze31_roll_precomp_%j.out
 #SBATCH --error=offline_reconstructor_logs/reco_teacher_joint_fusion_6model_150k75k150k/analyze31_roll_precomp_%j.err
 
@@ -136,4 +136,3 @@ echo "============================================================"
 printf ' %q' "${CMD[@]}"
 echo
 "${CMD[@]}"
-
