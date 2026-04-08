1ef8e9371c1bc5edf1e1a423fc29bc8f098f5e52
ryanlyang <ryan_l_yang@brown.edu>
2026-04-08 15:28:36 -0400

    good

--- sbatch/reco_teacher_joint_fusion_6model_150k75k150k/run_analyze_hlt_joint55_rolling_chunk_greedy_1m.sh
+++ sbatch/reco_teacher_joint_fusion_6model_150k75k150k/run_analyze_hlt_joint55_rolling_chunk_greedy_1m.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=an55roll1m
-#SBATCH --partition=debug
+#SBATCH --partition=tier3
 #SBATCH --gres=gpu:1
 #SBATCH --mem=64G
-#SBATCH --time=1-00:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --output=offline_reconstructor_logs/reco_teacher_joint_fusion_6model_150k75k150k/analyze55_roll1m_%j.out
 #SBATCH --error=offline_reconstructor_logs/reco_teacher_joint_fusion_6model_150k75k150k/analyze55_roll1m_%j.err
 
