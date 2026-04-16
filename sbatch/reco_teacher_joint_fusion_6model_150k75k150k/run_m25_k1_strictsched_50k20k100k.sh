edd970bd4b3daa54282aef8c1222e1475aa43299
ryanlyang <ryan_l_yang@brown.edu>
2026-04-16 14:45:07 -0400

    nice

--- sbatch/reco_teacher_joint_fusion_6model_150k75k150k/run_m25_k1_strictsched_50k20k100k.sh
+++ sbatch/reco_teacher_joint_fusion_6model_150k75k150k/run_m25_k1_strictsched_50k20k100k.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=debug
 #SBATCH --gres=gpu:1
 #SBATCH --mem=64G
-#SBATCH --time=23:00:00
+#SBATCH --time=24:00:00
 #SBATCH --output=offline_reconstructor_logs/reco_teacher_joint_fusion_6model_150k75k150k/m25_k1_50k20k100k_%j.out
 #SBATCH --error=offline_reconstructor_logs/reco_teacher_joint_fusion_6model_150k75k150k/m25_k1_50k20k100k_%j.err
 
@@ -17,10 +17,10 @@ SEED="${SEED:-0}"
 DEVICE="${DEVICE:-cuda}"
 NUM_WORKERS="${NUM_WORKERS:-1}"
 
-N_TRAIN_JETS="${N_TRAIN_JETS:-17000}"
-N_TRAIN_SPLIT="${N_TRAIN_SPLIT:-5000}"
-N_VAL_SPLIT="${N_VAL_SPLIT:-2000}"
-N_TEST_SPLIT="${N_TEST_SPLIT:-10000}"
+N_TRAIN_JETS="${N_TRAIN_JETS:-170000}"
+N_TRAIN_SPLIT="${N_TRAIN_SPLIT:-50000}"
+N_VAL_SPLIT="${N_VAL_SPLIT:-20000}"
+N_TEST_SPLIT="${N_TEST_SPLIT:-100000}"
 OFFSET_JETS="${OFFSET_JETS:-0}"
 MAX_CONSTITS="${MAX_CONSTITS:-100}"
 
@@ -50,7 +50,7 @@ CMD=(
   --num_workers "${NUM_WORKERS}"
   --seed "${SEED}"
   --reco_epochs 175
-  --reco_batch_size 64
+  --reco_batch_size 80
   --reco_lr 1.8e-4
   --reco_patience 30
   --reco_min_epochs 45
