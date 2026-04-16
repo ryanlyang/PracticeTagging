aae48b806aa48efcb4e009f23a9bef6770726dd2
ryanlyang <ryan_l_yang@brown.edu>
2026-04-16 17:46:08 -0400

    nice

--- sbatch/reco_teacher_joint_fusion_6model_150k75k150k/run_m28_k1_sinkset_noar_50k20k100k.sh
+++ sbatch/reco_teacher_joint_fusion_6model_150k75k150k/run_m28_k1_sinkset_noar_50k20k100k.sh
@@ -4,8 +4,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=64G
 #SBATCH --time=24:00:00
-#SBATCH --output=offline_reconstructor_logs/reco_teacher_joint_fusion_6model_150k75k150k/m28_k1_sinkset_noar_50k20k100k_%j.out
-#SBATCH --error=offline_reconstructor_logs/reco_teacher_joint_fusion_6model_150k75k150k/m28_k1_sinkset_noar_50k20k100k_%j.err
+#SBATCH --output=offline_reconstructor_logs/reco_teacher_joint_fusion_6model_150k75k150k/m28_k1_chamset_noar_50k20k100k_%j.out
+#SBATCH --error=offline_reconstructor_logs/reco_teacher_joint_fusion_6model_150k75k150k/m28_k1_chamset_noar_50k20k100k_%j.err
 
 set -euo pipefail
 
@@ -60,7 +60,7 @@ CMD=(
   --beam_temperature 0.9
   --num_hypotheses 1
   --joint_epochs 0
-  --set_loss_mode hungarian
+  --set_loss_mode chamfer
   --hungarian_shortlist_k 2
   --loss_w_ar 0.0
   --loss_w_set 1.0
