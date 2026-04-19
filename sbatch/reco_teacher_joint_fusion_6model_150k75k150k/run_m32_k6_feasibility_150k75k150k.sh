f0acc67acc97b19ec0dfb910c7f008f3b0c9ae91
ryanlyang <ryan_l_yang@brown.edu>
2026-04-19 17:46:07 -0400

    nice

--- sbatch/reco_teacher_joint_fusion_6model_150k75k150k/run_m32_k6_feasibility_150k75k150k.sh
+++ sbatch/reco_teacher_joint_fusion_6model_150k75k150k/run_m32_k6_feasibility_150k75k150k.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=tier3
 #SBATCH --gres=gpu:1
 #SBATCH --mem=64G
-#SBATCH --time=10-00:00:00
+#SBATCH --time=6-00:00:00
 #SBATCH --output=offline_reconstructor_logs/reco_teacher_joint_fusion_6model_150k75k150k/m32_k6_feasibility_%j.out
 #SBATCH --error=offline_reconstructor_logs/reco_teacher_joint_fusion_6model_150k75k150k/m32_k6_feasibility_%j.err
 
