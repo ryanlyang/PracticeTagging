691b84bdc5988b410083fe46888757c382c9b92c
ryanlyang <ryan_l_yang@brown.edu>
2026-04-20 15:42:34 -0400

    good

--- sbatch/reco_teacher_joint_fusion_6model_150k75k150k/run_m36_k3_dict1m_retrieval_dualview_150k75k300k.sh
+++ sbatch/reco_teacher_joint_fusion_6model_150k75k150k/run_m36_k3_dict1m_retrieval_dualview_150k75k300k.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=m36dret
-#SBATCH --partition=tier3
+#SBATCH --partition=debug
 #SBATCH --gres=gpu:1
-#SBATCH --mem=120G
-#SBATCH --time=7-00:00:00
+#SBATCH --mem=96G
+#SBATCH --time=1-00:00:00
 #SBATCH --output=offline_reconstructor_logs/reco_teacher_joint_fusion_6model_150k75k150k/m36_k3_dict1m_retrieval_dualview_%j.out
 #SBATCH --error=offline_reconstructor_logs/reco_teacher_joint_fusion_6model_150k75k150k/m36_k3_dict1m_retrieval_dualview_%j.err
 
