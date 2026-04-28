85e196c8ca09932df9d67bb8241e86d02853e9c8
ryanlyang <ryan_l_yang@brown.edu>
2026-04-28 15:19:17 -0400

    nice

--- sbatch/reco_teacher_joint_fusion_6model_150k75k150k/run_m38_k6_seeded_m28_detresid_multicand_carryboost_150k75k300k.sh
+++ sbatch/reco_teacher_joint_fusion_6model_150k75k150k/run_m38_k6_seeded_m28_detresid_multicand_carryboost_150k75k300k.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=tier3
 #SBATCH --gres=gpu:1
 #SBATCH --mem=96G
-#SBATCH --time=16-00:00:00
+#SBATCH --time=8-00:00:00
 #SBATCH --output=offline_reconstructor_logs/reco_teacher_joint_fusion_6model_150k75k150k/m38_k6_seeded_m28_detresid_multicand_carryboost_%j.out
 #SBATCH --error=offline_reconstructor_logs/reco_teacher_joint_fusion_6model_150k75k150k/m38_k6_seeded_m28_detresid_multicand_carryboost_%j.err
 
