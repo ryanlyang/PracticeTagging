d5eb0f74fac50585ab137121f99cac9ea9a0bd79
ryanlyang <ryan_l_yang@brown.edu>
2026-06-08 23:07:36 -0400

    nice

--- fresh_fusion_audit/run_fresh_fusion_audit_fixedhlt_filename_small.sh
+++ fresh_fusion_audit/run_fresh_fusion_audit_fixedhlt_filename_small.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=jcFrSm
 #SBATCH --partition=debug
-#SBATCH --time=9:00:00
+#SBATCH --time=7:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
